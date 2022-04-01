4e977366f63d87ec6ac4ddc42f3d4525b63460b8
Trisha M <70506440+Tri-M@users.noreply.github.com>
2022-04-01 15:28:30 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,15 @@
+for i in range(101):
+    if i%15==0:
+        print(i)
+        print("FizzBuzz")
+        continue
+    elif i%3==0:
+        print(i)
+        print("Fizz")
+        continue
+    elif i%5==0:
+        print(i)
+        print("Buzz")
+        continue
+    
+    
